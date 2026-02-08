ecea69b0298bef58abab8bd2337778bd6c716d04
Naramarr <naramaruthi2000@gmail.com>
2026-02-09 00:37:05 +0530

    Restructured project into src, data, artifacts folders

--- src/ml_model.py
+++ src/ml_model.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 import joblib
 
-df = pd.read_csv("heart.csv")
+df = pd.read_csv("data/heart.csv")
 
 df.dropna(inplace= True)
 df.drop_duplicates(inplace=True)
